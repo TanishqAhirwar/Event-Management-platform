b4497779e6aec546dec488d1e99ff21e39c8a30f
tanishqahirwar <tanishqahirwar258@gmail.com>
2026-06-23 12:32:42 +0530

    auth middleware completed

---
+++ backend/middleware/isAuthenticated.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import ApiResponse from "../response/pattern";
+
+const isAuthenticated = (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res
+        .status(401)
+        .json(new ApiResponse(false, null, "User not Authenticated"));
+    }
+
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+    req.id = decoded.userId;
+
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json(new ApiResponse(false,null, "Invalid or Expired Token"));
+  }
+};
+
+export default isAuthenticated;
