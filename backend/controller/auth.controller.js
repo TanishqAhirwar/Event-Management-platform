37613fac274cce2caa0a04a5c6949a37b8e037ec
tanishqahirwar <tanishqahirwar258@gmail.com>
2026-06-29 21:51:28 +0530

    all auth apis tested and working successfully on postman

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,11 +1,12 @@
-import User from "../model/userModel.js";
+import {User} from "../model/userModel.js";
 import ApiResponse from "../response/pattern.js";
+import Bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const userRegister = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { fullName, email, password } = req.body;
   try {
-    if (!username || !email || !password) {
+    if (!fullName || !email || !password) {
       return res
         .status(400)
         .json(new ApiResponse(false, null, "All Fields are required"));
@@ -19,16 +20,16 @@ export const userRegister = async (req, res) => {
         .json(new ApiResponse(false, null, "Email already exists"));
     }
 
-    let hashedPassword = await Bcrypt.hashPassword(password);
+    let hashedPassword = await Bcrypt.hash(password, 10);
     const user = await User.create({
-      username,
+      fullName,
       email: email.toLowerCase(),
       password: hashedPassword,
     });
 
     const userResponse = {
       id: user._id,
-      username: user.username,
+      fullName: user.fullName,
       email: user.email,
     };
 
@@ -36,7 +37,7 @@ export const userRegister = async (req, res) => {
       from: process.env.EMAIL_USER,
       to: email,
       subject: "Welcome Message",
-      html: `<h1>Dear ${user.username}</h1>
+      html: `<h1>Dear ${user.fullName}</h1>
            <b>welcome to our Website</b>`,
     };
 
@@ -68,7 +69,6 @@ export const userLogin = async (req, res) => {
     // Find User
     const user = await User.findOne({
       email: email.toLowerCase(),
-      isAdmin: false,
     });
 
     if (!user) {
@@ -78,7 +78,7 @@ export const userLogin = async (req, res) => {
     }
 
     // Compare Password
-    const isMatch = await Bcrypt.comparePassword(password, user.password);
+    const isMatch = await Bcrypt.compare(password, user.password);
 
     if (!isMatch) {
       return res
@@ -97,7 +97,7 @@ export const userLogin = async (req, res) => {
     // Response
     const userResponse = {
       _id: user._id,
-      username: user.username,
+      fullName: user.fullName,
       email: user.email,
       role: user.role,
     };
@@ -110,7 +110,6 @@ export const userLogin = async (req, res) => {
         sameSite: "strict",
       })
       .json(new ApiResponse(true, userResponse, "User Login successfully"));
-
   } catch (error) {
     console.error(error);
 
@@ -118,17 +117,19 @@ export const userLogin = async (req, res) => {
   }
 };
 
-async function getUserById(req, res) {
+export const logOut = async (req, res) => {
   try {
-    let user = await User.findById(req.data._id);
-
-    if (!user) {
-      return res.json(new ApiResponse(false, null, "Users not found"));
-    }
-
-    return res.json(new ApiResponse(true, user, "success"));
+    return res
+      .status(200)
+      .clearCookie("token", {
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json(new ApiResponse(true, null, "Logged Out Successfully"));
   } catch (error) {
     console.error(error);
-    return res.json(new ApiResponse(false, null, error.message));
+
+    return res.status(500).json(new ApiResponse(false, null, error.message));
   }
-}
+};
+
