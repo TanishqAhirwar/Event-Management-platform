85a2ec78b0ebafd7b7636af3fc641d6d4db4f4e3
tanishqahirwar <tanishqahirwar258@gmail.com>
2026-07-21 14:39:30 +0530

    all routes added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,23 @@
 import express from "express";
-import {userRegister,userLogin,logOut} from "../controller/auth.controller.js"
+import {userRegister, userLogin, logOut, verifyEmail, resetPassword, forgotPassword} from "../controller/auth.controller.js";
 
 const router = express.Router();
 
 router.route("/register").post(userRegister);
 router.route("/login").post(userLogin);
 router.route("/logout").get(logOut);
+router.get(
+    "/verify-email/:token",
+    verifyEmail);
+
+router.post(
+  "/forgot-password",
+  forgotPassword
+);
+
+router.post(
+  "/reset-password/:token",
+  resetPassword
+);
 
 export default router;
